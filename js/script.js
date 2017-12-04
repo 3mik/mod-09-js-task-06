a4d9027d5a122d4a0d1cf4a93fccab346a9e013e
Krzysztof Milek <krzysztof.milek73@gmail.com>
2017-12-04 18:24:03 +0100

    Solve task 9.6

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var list = document.getElementById('list'),
+    addButton = document.getElementById('js-addElem'),
+    countItem = 0;
+
+addButton.addEventListener('click', function(e) {
+countItem += 1;
+var li = document.createElement('li');
+li.innerHTML = 'item ' + countItem;
+list.appendChild(li);
+});
